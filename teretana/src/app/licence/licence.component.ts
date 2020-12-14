b0a00a42f2fb7e83b30953b695a97b9b3318bb29
Jana Jovicic <jana.jovicic755@gmail.com>
2020-12-14 17:18:13 +0100

    dodat formular za prijavu klijenta na program obuke za licencu

--- teretana/src/app/licence/licence.component.ts
+++ teretana/src/app/licence/licence.component.ts
@@ -11,8 +11,11 @@ export class LicenceComponent implements OnInit {
 
   public tipUlogovanog:string;
 
-  public addNewLicence = false;
+  public openNewLicenceForm = false;
   public newLicenceForm: FormGroup;
+
+  public openClientCheckInForm = false;
+  public clientCheckInForm: FormGroup;
 
   constructor(private korisnikService: KorisnikService,
               private formBuilder: FormBuilder) {
@@ -20,9 +23,9 @@ export class LicenceComponent implements OnInit {
 
     this.newLicenceForm = this.formBuilder.group({
       type: ['', [Validators.required]],
+      description: ['', [Validators.required]],
       trainers: ['', [Validators.required]],
       location: ['', [Validators.required]],
-      conditions: ['', [Validators.required]],
       requiredDocuments: ['', [Validators.required]],
       courseStart: ['', [Validators.required]],
       courseEnd: ['', [Validators.required]],
@@ -30,6 +33,15 @@ export class LicenceComponent implements OnInit {
       examDate: ['', [Validators.required]],
       examTimeStart: ['', [Validators.required]],
       examTimeFinish: ['', [Validators.required]],
+    });
+
+    this.clientCheckInForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      membershipCardNum: ['', [Validators.required]],
+      yearsInSport: ['', [Validators.required]],
+      competitionDoc: [''],
+      sportInfo: ['', [Validators.required]]
     });
   }
 
@@ -57,7 +69,11 @@ export class LicenceComponent implements OnInit {
   }
 
   public addNewLicenceClicked(){
-    this.addNewLicence = true;
+    this.openNewLicenceForm = true;
+  }
+
+  public checkInClicked(){
+    this.openClientCheckInForm = true;
   }
 
   public sendNewLicenceInfo(formData: any){
@@ -74,9 +90,35 @@ export class LicenceComponent implements OnInit {
     window.alert('Novi program obuke je uspešno dodat!');
     
     this.newLicenceForm.reset();
+    this.openNewLicenceForm = false;
   }
 
   public clearForm(){
     this.newLicenceForm.reset();
+    this.openNewLicenceForm = false;
   }
+
+
+  public sendClientInfo(formData: any){
+
+    console.log(formData)
+
+    if(!this.clientCheckInForm.valid){
+      window.alert('Formular nije validan. Ispravite podatke.');
+      return;
+    }
+
+    /* Ovde se salju podaci */
+
+    window.alert('Uspešno ste se prijavili!');
+    
+    this.clientCheckInForm.reset();
+    this.openClientCheckInForm = false;
+  }
+
+  public clearCheckInForm(){
+    this.clientCheckInForm.reset();
+    this.openClientCheckInForm = false;
+  }
+  
 }
